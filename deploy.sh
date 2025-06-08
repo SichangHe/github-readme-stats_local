a5ca2985c0652106547840b61db97b466c37225d
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2025-06-08 12:21:39 +0800

    tweak SVG

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,11 @@ sleep 5
 cd ..
 
 export PREFIX="localhost:9000"
-export URL_PATH="/top-langs/?username=sichanghe&langs_count=18&layout=compact&exclude_repo=CoreMLProto,reproduce_tensorflow_tensorflow_issue_61654,STATS401,learn_program,rails_tutorial,Notes_Steven,mdbook_katex_template,igem-2022-dku-backup,mdbook_fancy_theme,BigDecimal-Matrix-and-column-vector-calculator-in-Java&hide=Batchfile,CSS,Handlebars,HTML,Jupyter%20Notebook,Less,Tex,VBScript,Markdown,Shell"
+export URL_PATH="/top-langs/?username=sichanghe&langs_count=18&layout=compact&exclude_repo=CoreMLProto,reproduce_tensorflow_tensorflow_issue_61654,STATS401,learn_program,rails_tutorial,Notes_Steven,notes,mdbook_katex_template,igem-2022-dku-backup,mdbook_fancy_theme,BigDecimal-Matrix-and-column-vector-calculator-in-Java&hide=Batchfile,CSS,Handlebars,HTML,Jupyter%20Notebook,Less,Tex,VBScript,Markdown,Shell"
 echo "Downloading most owned languages SVG..."
-curl "$PREFIX$URL_PATH" -o most_owned_languages.svg
+export SVG_NAME="most_owned_languages.svg"
+curl "$PREFIX$URL_PATH" -o "$SVG_NAME"
+sed -i -e 's/Most Used/Most Owned/g'-e 's/Most Used/Most Owned/g' "$SVG_NAME"
 cat most_owned_languages.svg
 ls -lah most_owned_languages.svg
 
